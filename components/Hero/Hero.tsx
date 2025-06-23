8a3c2ac516121ffcf1a490acee2c188d6dab992b
Fred <huertaserick@yahoo.ie>
2025-06-23 15:46:32 +0100

    style: hero text and hero bg added using absolute positioning

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import SplitText from "../ReactBitsUI/SplitText";
 
 const handleAnimationComplete = () => {
@@ -7,8 +8,18 @@ const handleAnimationComplete = () => {
 
 const Hero = () => {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div className="container max-w-md lg:max-w-screen text-center">
+    <div className="relative h-screen flex justify-center items-center">
+      <div className="absolute bg-black opacity-70 inset-0 -z-40"></div>
+      <div className="absolute -z-50 w-full h-full">
+        <Image
+          className="w-full h-full object-cover"
+          width={1000}
+          height={1000}
+          src="/images/zenscape-hero.jpg"
+          alt="Welcome to Zenscape"
+        />
+      </div>
+      <div className="container z-10 max-w-md lg:max-w-screen text-center">
         <SplitText
           text="Welcome to Zenscape."
           className="text-7xl font-semibold text-center"
