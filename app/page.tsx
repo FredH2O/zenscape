b14bb91778aae7c03f6c4b56f8583f9a76d16161
Fred <huertaserick@yahoo.ie>
2025-06-24 15:01:39 +0100

    feat: navigation added, will add more content and also scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from "@/components/Hero/Hero";
+import Nav from "@/components/Nav/Nav";
 
 export default function Home() {
   return (
     <div>
+      <Nav />
       <Hero />
     </div>
   );
---
+++ components/Nav/Nav.tsx
@@ -0,0 +1,26 @@
+const NavMenu: string[] = [
+  "Home",
+  "Services",
+  "Reviews",
+  "Contact",
+  "About Us",
+];
+
+const Nav = () => {
+  return (
+    <nav className="fixed top-0 left-0 z-20 w-full py-15">
+      <ul className="flex space-x-5 justify-center">
+        {NavMenu.map((title, index) => (
+          <li
+            className="cursor-pointer hover:text-green-500 transition-all duration-300"
+            key={index}
+          >
+            <a href="">{title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
