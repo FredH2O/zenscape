3a41c97e423672223dd5c936cec093aab984ddf1
Fred <huertaserick@yahoo.ie>
2025-07-17 23:18:32 +0100

    style: styling for desktop, laptop and tablet fixed

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -8,11 +8,11 @@ const NavMenu: string[] = [
 
 const Nav = () => {
   return (
-    <nav className="border mx-auto py-4">
+    <nav className=" bg-emerald-500 text-white mx-auto py-4">
       <ul className="flex gap-5 justify-center text-xl">
         {NavMenu.map((title, index) => (
           <li
-            className="cursor-pointer hover:text-green-500 transition-all duration-300"
+            className="cursor-pointer hover:text-slate-500 transition-all duration-300"
             key={index}
           >
             <a href="">{title}</a>
--- components/TopBar/TopBar.tsx
+++ components/TopBar/TopBar.tsx
@@ -1,8 +1,23 @@
 import Image from "next/image";
+import {
+  FaShoppingCart as ShoppingCartIcon,
+  FaUser as UserIcon,
+} from "react-icons/fa";
+
+const userIcons = [
+  {
+    name: "Sign-in",
+    icon: <UserIcon className="text-2xl" />,
+  },
+  {
+    name: "Cart",
+    icon: <ShoppingCartIcon className="text-2xl" />,
+  },
+];
 
 const TopBar = () => {
   return (
-    <div className="px-5 py-3 border">
+    <div className="px-5 py-4 bg-emerald-50">
       <div className="container mx-auto flex justify-evenly items-center gap-10">
         <div className="flex justify-center items-center">
           <Image
@@ -11,22 +26,28 @@ const TopBar = () => {
             width={50}
             height={50}
           />
-          <h1 className="text-2xl italic font-bold">Zenscape</h1>
+          <h1 className="text-2xl italic font-bold text-slate-600">Zenscape</h1>
         </div>
         <div className="flex justify-center items-center w-[50%]">
           <input
             type="search"
-            className=" bg-white rounded-l-full active:bg-green-500 text-black px-5 py-2.5 placeholder:text-slate-400 w-full"
+            className=" bg-slate-500/50 rounded-l-full active:bg-green-500 text-white px-5 py-2.5 placeholder:text-white w-full"
             placeholder="Search Products..."
           />
-          <button className="bg-green-500 border-white border py-2.5 cursor-pointer px-3 rounded-r-full">
-            Button
+          <button className="bg-green-500/90 text-white hover:bg-green-500/50 transition-all ease-in-out duration-100 py-2.5 cursor-pointer px-3 rounded-r-full">
+            Search
           </button>
         </div>
 
-        <div className="flex gap-5">
-          <p>Sign In</p>
-          <p>€0.00</p>
+        <div className="flex gap-5 text-center">
+          {userIcons.map((user) => (
+            <button
+              key={user.name}
+              className="flex flex-col text-slate-600 justify-center items-center cursor-pointer"
+            >
+              {user.icon} {user.name}
+            </button>
+          ))}
         </div>
       </div>
     </div>
