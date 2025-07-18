b8a8abfde5d9917b7c102556c38cbc655e00f832
Fred <huertaserick@yahoo.ie>
2025-07-18 20:33:07 +0100

    feat: mobile nav working

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -2,6 +2,7 @@
 
 import { GiHamburgerMenu as BurgerMenu } from "react-icons/gi";
 import { IoMdClose } from "react-icons/io";
+import { motion } from "framer-motion";
 
 import { useState } from "react";
 
@@ -21,23 +22,10 @@ const Nav = () => {
   };
 
   return (
-    <nav className=" bg-emerald-500 text-white mx-auto py-4">
-      <ul className="md:flex gap-5 hidden justify-center text-xl">
-        {NavMenu.map((title, index) => (
-          <li
-            className="cursor-pointer hover:text-slate-500 transition-all duration-300"
-            key={index}
-          >
-            <a href="">{title}</a>
-          </li>
-        ))}
-      </ul>
-
-      <button onClick={handleToggleMenu}>
-        <BurgerMenu />
-      </button>
-      {burgerMenu && (
-        <ul className="flex gap-5 justify-center text-xl">
+    <nav className="relative bg-emerald-500 text-white mx-auto py-4">
+      {/* large screen and tablet */}
+      <div className="flex justify-center">
+        <ul className="md:flex gap-5 hidden justify-center text-xl">
           {NavMenu.map((title, index) => (
             <li
               className="cursor-pointer hover:text-slate-500 transition-all duration-300"
@@ -47,7 +35,44 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-      )}
+      </div>
+
+      {/* mobile screen */}
+      <div className="flex">
+        <button className="md:hidden ml-auto pr-5" onClick={handleToggleMenu}>
+          <BurgerMenu />
+        </button>
+        {burgerMenu && (
+          <div className="fixed top-0 w-full h-screen z-50">
+            <div
+              className="h-screen bg-black/50"
+              onClick={handleToggleMenu}
+            ></div>
+            <div className="fixed flex justify-start p-10 items-start top-0 h-screen bg-emerald-500 w-[70%]">
+              <button
+                onClick={handleToggleMenu}
+                className="absolute top-10 right-10"
+              >
+                <IoMdClose />
+              </button>
+              <motion.ul
+                initial={{ opacity: 0, x: -100 }}
+                animate={{ opacity: 1, x: 0 }}
+                className="flex flex-col md:hidden gap-5 justify-center text-xl"
+              >
+                {NavMenu.map((title, index) => (
+                  <li
+                    className="cursor-pointer hover:text-slate-500 transition-all duration-300"
+                    key={index}
+                  >
+                    <a href="">{title}</a>
+                  </li>
+                ))}
+              </motion.ul>
+            </div>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
