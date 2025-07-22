607f67e3f9838bdb2242a9691886186eeda6341a
Fred <huertaserick@yahoo.ie>
2025-07-22 20:05:22 +0100

    feat: styling changed for SellingPoint to make sure its responsive

--- components/SellingPoint/SellingPoint.tsx
+++ components/SellingPoint/SellingPoint.tsx
@@ -5,30 +5,32 @@ import AnimatedTeams from "@/components/AnimatedIcon/AnimatedTeams";
 const SellingPointArray = [
   {
     title: "Roots",
-    description: "Rooted since 1984 in Co.Offaly",
+    description: "Proudly established in County Offaly since 1984.",
     icons: <AnimatedOakTree />,
   },
   {
     title: "Variety",
-    description: "Selections like you've never seen before",
+    description: "An unparalleled selection to suit every taste and need.",
     icons: <AnimatedTeams />,
   },
   {
     title: "Delivery",
-    description: "Fast shipping, straight to your home",
+    description: "Reliable, fast shipping—directly to your doorstep.",
     icons: <AnimatedDelivery />,
   },
 ];
 
 const SellingPoint = () => {
   return (
-    <div className="bg-emerald-500">
-      <div className="max-w-7xl mx-auto bg-emerald-500 flex justify-between items-center gap-3">
+    <div className="bg-emerald-500/30 text-slate-600">
+      <div className="max-w-7xl mx-auto bg-transparent py-10 lg:px-15 px-10 flex flex-col lg:flex-row justify-between items-start gap-3">
         {SellingPointArray.map((point, index) => (
-          <div key={index} className="border px-3">
-            <div className="w-15">{point.icons}</div>
-            <h3 className="text-2xl">{point.title}</h3>
-            <p>{point.description}</p>
+          <div key={index} className="flex justify-center items-center gap-5">
+            <div className="w-20">{point.icons}</div>
+            <div className="space-y-1">
+              <h3 className="text-xl xl:text-2xl">{point.title}</h3>
+              <p className="text-sm lg:text-[15px]">{point.description}</p>
+            </div>
           </div>
         ))}
       </div>
