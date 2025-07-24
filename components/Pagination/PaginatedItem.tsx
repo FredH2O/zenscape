3e466dd815b9a91cc98afe24c9c68ddbf3611196
Fred <huertaserick@yahoo.ie>
2025-07-24 20:40:00 +0100

    style: changed styling for pagination, made it bigger, fixed a bug when index goes -1 it will crash the app too

--- components/Pagination/PaginatedItem.tsx
+++ components/Pagination/PaginatedItem.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import product from "@/data/products";
 import Image from "next/image";
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 
 const windowSize = 3;
 
@@ -13,7 +14,9 @@ const PaginatedItems = () => {
   };
 
   const handlePrevious = () => {
-    setStartIndex((prevIndex) => (prevIndex - 1) % product.length);
+    setStartIndex(
+      (prevIndex) => (prevIndex - 1 + product.length) % product.length
+    );
   };
 
   const visibleItems = Array.from({ length: windowSize }, (_, i) => {
@@ -23,41 +26,40 @@ const PaginatedItems = () => {
 
   return (
     <div className="max-w-4xl mx-auto py-10">
-      <h1 className="text-2xl font-bold mb-6 text-center">
+      <h1 className="text-2xl text-slate-600 font-bold mb-6 text-center">
         🌿 Plant Collection
       </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {visibleItems.map((product) => (
-          <div
-            key={product.id}
-            className="bg-white shadow-md rounded-xl p-4 text-center"
-          >
-            <Image
-              src={product.image}
-              alt={product.alt}
-              className="w-full h-40 object-cover rounded-md mb-3"
-              width={1000}
-              height={1000}
-            />
-            <h2 className="font-semibold">{product.title}</h2>
-            <p className="text-green-600 font-bold">€{product.price}</p>
-          </div>
-        ))}
-      </div>
-
-      <div className="flex justify-center items-center gap-4 mt-8">
+      <div className="relative flex justify-center items-center">
         <button
           onClick={handlePrevious}
-          className="bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded-lg"
+          className="text-slate-600 cursor-pointer px-4 py-2 rounded-lg"
         >
-          ⬅️ Prev
+          <FaArrowCircleLeft />
         </button>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {visibleItems.map((product) => (
+            <div
+              key={product.id}
+              className="bg-white shadow-md rounded-xl p-4 text-center"
+            >
+              <Image
+                src={product.image}
+                alt={product.alt}
+                className="h-90 object-cover rounded-md mb-3"
+                width={1000}
+                height={1000}
+              />
+              <h2 className="font-semibold text-slate-600">{product.title}</h2>
+              <p className="text-green-600 font-bold">€{product.price}</p>
+            </div>
+          ))}
+        </div>
         <button
           onClick={handleNext}
-          className="bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded-lg"
+          className="text-slate-600 cursor-pointer px-4 py-2 rounded-lg"
         >
-          Next ➡️
+          <FaArrowCircleRight />
         </button>
       </div>
     </div>
