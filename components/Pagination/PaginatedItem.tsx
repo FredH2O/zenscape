6a60e9ab0cc22010aec5e7be9e30d7e1e752bb32
Fred <huertaserick@yahoo.ie>
2025-07-25 21:53:35 +0100

    feat: pagination have sense of direction now

--- components/Pagination/PaginatedItem.tsx
+++ components/Pagination/PaginatedItem.tsx
@@ -4,16 +4,20 @@ import product from "@/data/products";
 import Image from "next/image";
 import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 import useResponsiveWindowSize from "@/hooks/useResponsiveWindowSize";
+import { AnimatePresence, easeInOut, motion } from "framer-motion";
 
 const PaginatedItems = () => {
   const [startIndex, setStartIndex] = useState<number>(0);
+  const [direction, setDirection] = useState<"left" | "right">("right");
   const windowSize = useResponsiveWindowSize();
 
   const handleNext = () => {
+    setDirection("right");
     setStartIndex((prevIndex) => (prevIndex + 1) % product.length);
   };
 
   const handlePrevious = () => {
+    setDirection("left");
     setStartIndex(
       (prevIndex) => (prevIndex - 1 + product.length) % product.length
     );
@@ -37,24 +41,33 @@ const PaginatedItems = () => {
         >
           <FaArrowCircleLeft />
         </button>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {visibleItems.map((product) => (
-            <div
-              key={product.id}
-              className="bg-white shadow-md rounded-xl p-4 text-center"
-            >
-              <Image
-                src={product.image}
-                alt={product.alt}
-                className="h-90 object-cover rounded-md mb-3"
-                width={1000}
-                height={1000}
-              />
-              <h2 className="font-semibold text-slate-600">{product.title}</h2>
-              <p className="text-green-600 font-bold">€{product.price}</p>
-            </div>
-          ))}
-        </div>
+        <AnimatePresence mode="wait">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {visibleItems.map((product) => (
+              <motion.div
+                initial={{ opacity: 0, x: direction === "right" ? 50 : -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.3, ease: easeInOut }}
+                exit={{ opacity: 0, x: direction === "right" ? -50 : 50 }}
+                key={product.id}
+                className="bg-white shadow-md rounded-xl p-4 text-center"
+              >
+                <Image
+                  src={product.image}
+                  alt={product.alt}
+                  className="h-90 object-cover rounded-md mb-3"
+                  width={1000}
+                  height={1000}
+                />
+                <h2 className="font-semibold text-slate-600">
+                  {product.title}
+                </h2>
+                <p className="text-green-600 font-bold">€{product.price}</p>
+              </motion.div>
+            ))}
+          </div>
+        </AnimatePresence>
+
         <button
           onClick={handleNext}
           className="text-slate-600 text-xl cursor-pointer px-4 py-2 rounded-lg"
