6cdde26434ac5cc211af7c456b3eb4196a48124d
Fred <huertaserick@yahoo.ie>
2025-07-29 21:16:32 +0100

    feat: browse component done, and ready to be connected to browsing section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import BrowseOurRange from "@/components/BrowseOurRange/BrowseOurRange";
 import Hero from "@/components/Hero/Hero";
 import PaginatedItems from "@/components/Pagination/PaginatedItem";
 import SeasonalCollection from "@/components/SeasonalCollection/SeasonalCollection";
@@ -8,13 +9,16 @@ export default function Home() {
   return (
     <div className="">
       <Hero />
-      <div className="bg-emerald-50">
+      <div className="bg-emerald-50 border pb-10">
         <TestimonialBlock />
         <SellingPoint />
-        <section className="max-w-5xl mx-auto space-y-10 py-10">
-          <PaginatedItems />
-          <SeasonalCollection />
-        </section>
+        <div className="max-w-7xl mx-auto flex flex-col justify-center items-center">
+          <div className="max-w-5xl grid grid-col-1 gap-10 py-10">
+            <PaginatedItems />
+            <SeasonalCollection />
+          </div>
+          <BrowseOurRange />
+        </div>
       </div>
     </div>
   );
