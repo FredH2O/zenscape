4912b8d8fc1d9bf8a6162520fb5d1d8f6980bc25
Fred <huertaserick@yahoo.ie>
2025-08-07 19:52:11 +0100

    feat(plantwithus): fix image corner overflow and apply responsive styling

--- components/PlantWithUs/PlantWithUs.tsx
+++ components/PlantWithUs/PlantWithUs.tsx
@@ -3,20 +3,24 @@ import Image from "next/image";
 const PlantWithUs = () => {
   return (
     <section className="text-slate-700 px-5">
-      <div className="flex flex-col lg:flex-row">
+      <div className="flex rounded-md overflow-hidden flex-col lg:flex-row">
         <Image
+          className="object-cover w-full h-[600px]"
           width={500}
           height={250}
           src="/images/plant-with-us.jpg"
           alt="Our team are dedicated for the future."
         />
-        <div className="border flex flex-col justify-center items-center p-5">
-          <h2 className="lg:text-5xl text-4xl font-semibold">
-            Plant a Tree — Nurture the Future
-          </h2>
-          <h3 className="text-2xl mt-2 mb-4 text-gray-700/80">
-            A Fundraising Initiative for a Greener Tomorrow
-          </h3>
+        <div className="bg-emerald-500/20 flex flex-col space-y-5 text-center justify-center items-center md:p-10 lg:p-15 p-5">
+          <div>
+            <h2 className="lg:text-5xl text-4xl font-bold">
+              Plant a Tree — Nurture the Future
+            </h2>
+            <h3 className="text-xl mt-2 mb-4 text-gray-700/50 font-semibold">
+              A Fundraising Initiative for a Greener Tomorrow
+            </h3>
+          </div>
+
           <p className="leading-relaxed text-lg text-gray-600">
             With a contribution of just €10, you can become part of a lasting
             legacy. We will plant a native tree in County Offaly on your behalf
