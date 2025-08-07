2fcdab0a3e81291a48af83e305f77856cd99d25a
Fred <huertaserick@yahoo.ie>
2025-08-07 20:49:37 +0100

    feat(footer): add base footer component with structure

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,68 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <p>
+          Zenscape has been supplying plants by mail order since 1986 and
+          selling online since 2016.
+        </p>
+        <ul>
+          <li>Zenscape</li>
+          <li>Kealkill</li>
+          <li>Bantry</li>
+          <li>Co Offaly</li>
+          <li>Ireland</li>
+          <li>O11 J891</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Information</h3>
+        <ul>
+          <li>
+            <a href="">Search</a>
+          </li>
+          <li>
+            <a href="">Advice</a>
+          </li>
+          <li>
+            <a href="">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="">Refund Policy</a>
+          </li>
+          <li>
+            <a href="">Shipping Policy</a>
+          </li>
+          <li>
+            <a href="">Terms of Service</a>
+          </li>
+          <li>
+            <a href="">Copyrights</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Gardening Advice</h3>
+        <ul>
+          <li>
+            <a href="">Potted vs Bareroot</a>
+          </li>
+          <li>
+            <a href="">How to plant a tree</a>
+          </li>
+          <li>
+            <a href="">Fruit Tree Rootstocks</a>
+          </li>
+          <li>
+            <a href="">Pruning Apple Trees</a>
+          </li>
+          <li>
+            <a href="">Resources</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
